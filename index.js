a6443e8d4254c1aa60a22a03f12d7502ef94e892
Roan van Adrichem <roanvanadrichem98@live.nl>
2017-03-23 10:06:44 +0100

    mastermind

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var colors = ["red", "blue", "green", "purple", "yellow", "orange"];
+
+var combination = [];
+var combinationlength = 4;
+
+for (i = 0; i < combinationlength; i++) {
+
+number = Math.floor((Math.random() * 6));
+combination.push(number)
+}
+
+console.log(combination);
+
+for (i = 0; i < colors; i++) {
+
+}
